facc9053027720ace72b66702cc3f55c322f2bc9
somervda on dev pc <davidsomerville@comcast.net>
2019-06-20 10:16:59 -0400

    Updated and tested cypress test suite. Turned off PWA and firestore persistance to check that the cypress tests work.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,75 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+// *******************************************************************************
+// Component test commands (Reused through test suite)
+//  - checks for component access and component content
+// *******************************************************************************
+
+Cypress.Commands.add("verifyAboutComponent", () => {
+  cy.get("#mainMenu").click();
+  cy.get("#mainMenuAbout").click();
+  // Verify component was rendered
+  cy.url().should("include", "about");
+  cy.get(".mat-card-title").contains("About");
+});
+
+Cypress.Commands.add("verifyHomeComponent", () => {
+  cy.get("#mainMenu").click();
+  cy.get("#mainMenuHome").click();
+  // Verify component was rendered
+  cy.get(".mat-card-title").contains("Decisions");
+});
+
+Cypress.Commands.add("verifyMyProfileComponent", () => {
+  cy.get("#mainMenu").click();
+  cy.get("#mainMenuMyProfile").click();
+  // Verify component was rendered
+  cy.get("h1").contains("User Profile");
+});
+
+Cypress.Commands.add("verifyAdminComponent", () => {
+  cy.get("#mainMenu").click();
+  cy.get("#mainMenuAdministration").click();
+  cy.get("h1").contains("Administration");
+  cy.get('[routerlink="/users"]').click();
+  cy.get("h1").contains("Users");
+  cy.contains(Cypress.env("nonAdminUser").toLowerCase()).click();
+  cy.url().should("include", "user/");
+  cy.get("h1").contains("User Profile");
+});
+
+Cypress.Commands.add("verifyLogout", () => {
+  cy.get("#mainMenu").click();
+  cy.get("#mainMenuLogout").click();
+  // Verify user was logged out
+  cy.get(".user-avatar").should("not.exist");
+});
+
+Cypress.Commands.add("verifyNotAdministrator", () => {
+  cy.get("#mainMenu").click();
+  cy.get("#mainMenuHome").should("exist");
+  cy.get("#mainMenuAdministration").should("not.exist");
+  cy.get(".mat-drawer-backdrop").click();
+});
+
+Cypress.Commands.add("verifyNotActivated", () => {
+  cy.get("#mainMenu").click();
+  cy.get("#mainMenuHome").should("exist");
+  cy.get("#mainMenuMyProfile").should("not.exist");
+  cy.get(".mat-drawer-backdrop").click();
+});
+
+Cypress.Commands.add("logonEmail", (usercode, password) => {
+  cy.log("logonEmail");
+  cy.get("#mainMenu").click();
+  cy.get("#mainMenuLogin").click();
+  cy.contains("Sign in with email");
+  cy.get(":nth-child(2) > .firebaseui-idp-button").click();
+  cy.get(".firebaseui-id-submit").click();
+  cy.get(".mdl-textfield__input").type(usercode);
+  cy.get(".firebaseui-id-submit").click();
+  cy.get(":nth-child(3) > .mdl-textfield__input").type(password);
+  cy.get(".firebaseui-id-submit").click();
+});
