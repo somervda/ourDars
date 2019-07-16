77ef11eca4b8bac6ac9ee1100b7625f3a3ed60f8
David  on Go <davidsomerville@comcast.net>
2019-07-16 07:57:28 -0400

    wip darsolutions

--- src/app/services/darsolution.service.ts
+++ src/app/services/darsolution.service.ts
@@ -1,9 +1,65 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore , DocumentReference} from '@angular/fire/firestore';
+import { Darsolution } from '../models/darsolution.model';
+import { Observable } from 'rxjs';
+import { convertSnap, convertSnaps, dbFieldUpdate } from './db-utils';
+import OrderByDirection = firebase.firestore.OrderByDirection;
+import { map, first } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DarsolutionService {
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) {}
+
+  findById(did: string,dsid : string): Observable<Darsolution> {
+    return this.afs
+      .doc("/Dars/" + did + "/DarSolutions/" + dsid)
+      .snapshotChanges()
+      .pipe(
+        map(snap => {
+          return convertSnap<Darsolution>(snap);
+        })
+      );
+  }
+
+  findDarsolutions(
+    did : string,
+    filter = "",
+    sortField,
+    sortOrder: OrderByDirection,
+    pageSize
+  ): Observable<Darsolution[]> {
+    // console.log("findDarsolutions", sortField, sortOrder, pageSize);
+    return this.afs
+    .collection("dars").doc(did).collection("darSolutions", ref =>
+      ref.orderBy(sortField, sortOrder).limit(pageSize)
+  )
+      .snapshotChanges()
+      .pipe(
+        map(snaps => {
+          console.log("findDarsolutions", convertSnaps<Darsolution>(snaps));
+          return convertSnaps<Darsolution>(snaps);
+        })
+      );
+  }
+
+  fieldUpdate(did: string ,dsid: string, fieldName: string, newValue: any) {
+    if (did && dsid && fieldName) {
+      const updateObject = {};
+      dbFieldUpdate("/dars/" + did + "/darsolutions/" + dsid, fieldName, newValue, this.afs);
+    }
+  }
+
+  createDarsolution(did: string, darsolution: Darsolution): Promise<DocumentReference> {
+    return this.afs.collection("/dars/" + did + "/darsolutions/").add(darsolution);
+  }
+
+  deleteDarsolution(did:string, dsid: string): Promise<void> {
+    return this.afs
+      .collection("/dars/" + did + "/darsolutions/")
+      .doc(dsid)
+      .delete();
+  }
 }
