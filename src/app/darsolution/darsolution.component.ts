574af49b9bd2e1c7bdc30e2e62c7c4343dcedd54
David on big surface <davidsomerville@comcast.net>
2019-07-21 09:05:39 -0400

    wip - fixxing @Input problem

--- src/app/darsolution/darsolution.component.ts
+++ src/app/darsolution/darsolution.component.ts
@@ -1,44 +1,40 @@
-import { DarsolutionService } from './../services/darsolution.service';
-import { DarService } from './../services/dar.service';
-import { Component, OnInit, Input, OnDestroy, OnChanges } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Darsolution } from '../models/darsolution.model';
+import { DarsolutionService } from "./../services/darsolution.service";
+import { DarService } from "./../services/dar.service";
+import { Component, OnInit, Input, OnDestroy, OnChanges } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { Darsolution } from "../models/darsolution.model";
 import { firestore } from "firebase/app";
-import { Crud } from '../models/global.model';
-import { Observable, Subscription } from 'rxjs';
-import { MatSnackBar } from '@angular/material';
+import { Crud } from "../models/global.model";
+import { Observable, Subscription } from "rxjs";
+import { MatSnackBar } from "@angular/material";
 
 @Component({
-  selector: 'app-darsolution',
-  templateUrl: './darsolution.component.html',
-  styleUrls: ['./darsolution.component.scss']
+  selector: "app-darsolution",
+  templateUrl: "./darsolution.component.html",
+  styleUrls: ["./darsolution.component.scss"]
 })
-
 export class DarsolutionComponent implements OnInit, OnDestroy, OnChanges {
   @Input() did: string;
   @Input() dsid: string;
   @Input() crudAction: Crud;
-  @Input() dummy: any;
+  @Input() dummyValue: number;
   Crud = Crud;
   form: FormGroup;
-  darsolution : Darsolution;
-  darsolution$ : Subscription;
+  darsolution: Darsolution;
+  darsolution$: Subscription;
 
   constructor(
     private fb: FormBuilder,
-    private darService : DarService,
-    private darsolutionService : DarsolutionService,
+    private darService: DarService,
+    private darsolutionService: DarsolutionService,
     private snackBar: MatSnackBar
-    ) { 
-
-  }
+  ) {}
 
   ngOnInit() {
     console.log("darsolution onInit");
     if (!this.darsolution)
-      this.darsolution = { name: "", description: "",evalutionNotes: "" };
-    if (!this.crudAction)
-      this.crudAction = Crud.Create;
+      this.darsolution = { name: "", description: "", evalutionNotes: "" };
+    if (!this.crudAction) this.crudAction = Crud.Create;
     // if (this.crudAction == Crud.Update || this.crudAction == Crud.Delete)
     // {
     //   // load darsolution from firestore
@@ -49,85 +45,106 @@ export class DarsolutionComponent implements OnInit, OnDestroy, OnChanges {
     //   });
     // }
 
-  // Create form group and initialize with  values
-  this.form = this.fb.group({
-    name: [this.darsolution.name, [Validators.required,  Validators.minLength(10),Validators.maxLength(40)]],
-    description: [
-      this.darsolution.description,
-      [Validators.required, Validators.minLength(20), Validators.maxLength(1000)]
-    ],
-    evalutionNotes: [
-      this.darsolution.evalutionNotes,
-      [ Validators.maxLength(1000)]
-    ]
-  });
-  // for (const field in this.form.controls) {
-  //   this.form.get(field).markAsTouched();
-  // }
+    // Create form group and initialize with  values
+    this.form = this.fb.group({
+      name: [
+        this.darsolution.name,
+        [
+          Validators.required,
+          Validators.minLength(10),
+          Validators.maxLength(40)
+        ]
+      ],
+      description: [
+        this.darsolution.description,
+        [
+          Validators.required,
+          Validators.minLength(20),
+          Validators.maxLength(1000)
+        ]
+      ],
+      evalutionNotes: [
+        this.darsolution.evalutionNotes,
+        [Validators.maxLength(1000)]
+      ]
+    });
+    // for (const field in this.form.controls) {
+    //   this.form.get(field).markAsTouched();
+    // }
   }
 
   ngOnChanges() {
-    console.log("ngOnChanges did:",this.did," dsid:",this.dsid, this.crudAction);
+    console.log(
+      "ngOnChanges did:",
+      this.did,
+      " dsid:",
+      this.dsid,
+      " crudAction:",
+      this.crudAction,
+      " dummyvalue:",
+      this.dummyValue
+    );
     if (this.crudAction == Crud.Update || this.crudAction == Crud.Delete) {
       if (this.darsolution$) this.darsolution$.unsubscribe();
 
-      this.darsolution$ = this.darsolutionService.findById(this.did,this.dsid)
-      .subscribe(ds => {
-        console.log("darsolution subscription",ds);
-        this.darsolution = ds;
-        this.form.patchValue(this.darsolution);
-      });
-    }
-    else {
-
-      this.darsolution = { name: "", description: "",evalutionNotes: "" };
+      this.darsolution$ = this.darsolutionService
+        .findById(this.did, this.dsid)
+        .subscribe(ds => {
+          console.log("darsolution subscription", ds);
+          this.darsolution = ds;
+          this.form.patchValue(this.darsolution);
+        });
+    } else {
+      this.darsolution = { name: "", description: "", evalutionNotes: "" };
       this.form.patchValue(this.darsolution);
     }
-
   }
 
   onFieldUpdate(fieldName: string, toType?: string) {
-    if (
-      this.form.get(fieldName).valid &&
-      this.darsolution.id != "" 
-    ) {
+    if (this.form.get(fieldName).valid && this.darsolution.id != "") {
       let newValue = this.form.get(fieldName).value;
       // Do any type conversions before storing value
       if (toType && toType == "Timestamp")
-        newValue = firestore.Timestamp.fromDate(
-          this.form.get(fieldName).value
-        );
-      this.darsolutionService.fieldUpdate(this.did,this.dsid, fieldName, newValue);
+        newValue = firestore.Timestamp.fromDate(this.form.get(fieldName).value);
+      this.darsolutionService.fieldUpdate(
+        this.did,
+        this.dsid,
+        fieldName,
+        newValue
+      );
     }
   }
 
-  onCreate () {
-    console.log("onCreate",this.dummy);
+  onCreate() {
+    console.log("onCreate", this.dummyValue);
     for (const field in this.form.controls) {
       this.darsolution[field] = this.form.get(field).value;
     }
-    this.darsolutionService.createDarsolution(this.did,this.darsolution).then(docRef => {
-      this.snackBar.open("Solution '" + this.darsolution.name + "' created.", "", {
-        duration: 2000
+    this.darsolutionService
+      .createDarsolution(this.did, this.darsolution)
+      .then(docRef => {
+        this.snackBar.open(
+          "Solution '" + this.darsolution.name + "' created.",
+          "",
+          {
+            duration: 2000
+          }
+        );
+      })
+      .catch(function(error) {
+        console.error("Error creating solution: ", error);
       });
-      this.crudAction = Crud.Update;
-      this.dsid = docRef.id;
-    })
-    .catch(function(error) {
-      console.error("Error creating solution: ", error);
-    });
   }
 
   onDelete() {
     console.log("onDelete");
     const name = this.darsolution.name;
     this.darsolutionService
-      .deleteDarsolution(this.did,this.dsid)
+      .deleteDarsolution(this.did, this.dsid)
       .then(() => {
         this.snackBar.open("Solution '" + name + "' deleted!", "", {
           duration: 2000
         });
-        this.dsid = "";
       })
       .catch(function(error) {
         console.error("Error deleting solution: ", error);
@@ -137,9 +154,4 @@ export class DarsolutionComponent implements OnInit, OnDestroy, OnChanges {
   ngOnDestroy() {
     if (this.darsolution$) this.darsolution$.unsubscribe();
   }
-
-
-
-
-
 }
--- src/app/darsolutions/darsolutions.component.ts
+++ src/app/darsolutions/darsolutions.component.ts
@@ -1,27 +1,27 @@
-import { Subscription,  Observable } from 'rxjs';
-import { DarsolutionService } from './../services/darsolution.service';
-import { DarService } from './../services/dar.service';
-import { Component, OnInit, NgZone, OnDestroy } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { FormBuilder } from '@angular/forms';
-import { MatSnackBar } from '@angular/material';
-import { Dar } from '../models/dar.model';
-import { Darsolution } from '../models/darsolution.model';
-import { Crud } from '../models/global.model';
+import { Subscription, Observable } from "rxjs";
+import { DarsolutionService } from "./../services/darsolution.service";
+import { DarService } from "./../services/dar.service";
+import { Component, OnInit, NgZone, OnDestroy } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { FormBuilder } from "@angular/forms";
+import { MatSnackBar } from "@angular/material";
+import { Dar } from "../models/dar.model";
+import { Darsolution } from "../models/darsolution.model";
+import { Crud } from "../models/global.model";
 
 @Component({
-  selector: 'app-darsolutions',
-  templateUrl: './darsolutions.component.html',
-  styleUrls: ['./darsolutions.component.scss']
+  selector: "app-darsolutions",
+  templateUrl: "./darsolutions.component.html",
+  styleUrls: ["./darsolutions.component.scss"]
 })
-export class DarsolutionsComponent implements OnInit , OnDestroy{
-  dar : Dar;
-  darsolutions$ : Subscription;
-  darsolutions : Observable<Darsolution[]>;
-  displayedColumns: string[] = ['name','id'];
-  dsid : string;
+export class DarsolutionsComponent implements OnInit, OnDestroy {
+  dar: Dar;
+  darsolutions$: Subscription;
+  darsolutions: Observable<Darsolution[]>;
+  displayedColumns: string[] = ["name", "id"];
+  dsid: string;
   crudAction = Crud.Update;
-
+  forCD = 0;
 
   constructor(
     private route: ActivatedRoute,
@@ -29,43 +29,47 @@ export class DarsolutionsComponent implements OnInit , OnDestroy{
     private snackBar: MatSnackBar,
     private ngZone: NgZone,
     private router: Router,
-    private darsolutionservice : DarsolutionService) { }
+    private darsolutionservice: DarsolutionService
+  ) {}
 
   ngOnInit() {
     this.dar = this.route.snapshot.data["dar"];
     // get a observable of all darsolutions related to this dar
-    this.darsolutions = this.darsolutionservice.findAllDarsolutions(this.dar.id,100);
-    console.log("darsolutions$",this.darsolutions$);
+    this.darsolutions = this.darsolutionservice.findAllDarsolutions(
+      this.dar.id,
+      100
+    );
+    console.log("darsolutions$", this.darsolutions$);
   }
 
-  selectSolution(dsid : string) {
+  selectSolution(dsid: string) {
     this.crudAction = Crud.Update;
     this.dsid = dsid;
-    console.log("selectSolution"," crudAction:",this.crudAction," did:",this.dar.id," dsid:",this.dsid);
-
+    console.log(
+      "selectSolution",
+      " crudAction:",
+      this.crudAction,
+      " did:",
+      this.dar.id,
+      " dsid:",
+      this.dsid
+    );
   }
 
-  selectDeleteSolution(dsid : string) {
+  selectDeleteSolution(dsid: string) {
     this.crudAction = Crud.Delete;
     this.dsid = dsid;
-
   }
 
   onCreateNew() {
-    console.log("oncreatenew");
+    this.forCD++;
     // Toggle action to for an onChange event in the darSolution component
     this.crudAction = Crud.Create;
-    this.dsid = undefined;
-  }
-
-  getCurrentDate() : number {
-    const now = new Date();
-    return now.getUTCMilliseconds();
+    console.log("oncreatenew", this.crudAction, this.dsid, this.forCD);
   }
 
   ngOnDestroy() {
-    console.log("unsubscribe darsolutions",this.darsolutions$);
+    console.log("unsubscribe darsolutions", this.darsolutions$);
     if (this.darsolutions$) this.darsolutions$.unsubscribe();
   }
-
 }
