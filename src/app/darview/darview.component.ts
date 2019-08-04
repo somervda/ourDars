b8c72261b73c821e4173783a328c915fb1ccd508
David <39265971+somervda@users.noreply.github.com>
2019-08-04 07:51:15 -0400

    wip

--- src/app/darview/darview.component.ts
+++ src/app/darview/darview.component.ts
@@ -5,6 +5,12 @@ import { ActivatedRoute } from "@angular/router";
 import { TeamService } from "../services/team.service";
 import { Observable, Subscription } from "rxjs";
 import { Team } from "../models/team.model";
+import { Daruser } from "../models/daruser.model";
+import { DaruserService } from "../services/daruser.service";
+import { DarsolutionService } from "../services/darsolution.service";
+import { Darsolution } from "../models/darsolution.model";
+import { Darcriteria, CriteriaWeighting } from "../models/darcriteria.model";
+import { DarcriteriaService } from "../services/darcriteria.service";
 
 @Component({
   selector: "app-darview",
@@ -18,11 +24,18 @@ export class DarviewComponent implements OnInit, OnDestroy {
   team: Observable<Team>;
   dar$: Subscription;
   did: string;
+  darUsers: Observable<Daruser[]>;
+  darSolutions: Observable<Darsolution[]>;
+  darCriterias: Observable<Darcriteria[]>;
+  CriteriaWeighting = CriteriaWeighting;
 
   constructor(
     private route: ActivatedRoute,
     private teamService: TeamService,
-    private darService: DarService
+    private darService: DarService,
+    private daruserService: DaruserService,
+    private darsolutionService: DarsolutionService,
+    private darcriteriaService: DarcriteriaService
   ) {}
 
   ngOnInit() {
@@ -32,6 +45,15 @@ export class DarviewComponent implements OnInit, OnDestroy {
     this.dar$ = this.darService.findById(this.did).subscribe(dar => {
       this.dar = dar;
       this.team = this.teamService.findById(this.dar.tid);
+      this.darUsers = this.daruserService.findAllDarusers(this.did, 1000);
+      this.darSolutions = this.darsolutionService.findAllDarsolutions(
+        this.did,
+        1000
+      );
+      this.darCriterias = this.darcriteriaService.findAllDarcriteria(
+        this.did,
+        1000
+      );
     });
   }
 
