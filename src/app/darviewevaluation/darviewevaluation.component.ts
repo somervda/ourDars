ee41a1e68346351fa89af02f959c3e5e03ca29d5
David on big surface <davidsomerville@comcast.net>
2019-08-10 10:19:26 -0400

    wip evaluation matrix

--- src/app/darviewevaluation/darviewevaluation.component.ts
+++ src/app/darviewevaluation/darviewevaluation.component.ts
@@ -4,6 +4,9 @@ import { Dar } from "../models/dar.model";
 import { EvaluationScore, Darevaluation } from "../models/darevaluation.model";
 import { Subscription, Observable } from "rxjs";
 import { DarsolutionService } from "../services/darsolution.service";
+import { Darsolution } from "../models/darsolution.model";
+import { Darcriteria } from "../models/darcriteria.model";
+import { DarcriteriaService } from "../services/darcriteria.service";
 
 @Component({
   selector: "app-darviewevaluation",
@@ -13,39 +16,47 @@ import { DarsolutionService } from "../services/darsolution.service";
 export class DarviewevaluationComponent implements OnInit, OnDestroy {
   @Input() dar: Dar;
   darsolution$$: Subscription;
-  evaluations$$: Subscription;
-  e$: Observable<Darevaluation[]>;
+  darsolutions: Darsolution[];
+  darcriteria$$: Subscription;
+  darcriterias: Darcriteria[];
+  darevaluation$$: Subscription;
+  darevaluations: Darevaluation[];
 
   constructor(
     private darsolutionService: DarsolutionService,
+    private darcriteriaService: DarcriteriaService,
     private darevaluationService: DarevaluationService
   ) {}
 
   ngOnInit() {
-    // // Build the evaluationMatrix from criteris/solution/evaluation documents
-    // this.darsolution$$ = this.darsolutionService
-    //   .findAllDarsolutions(this.dar.id, 1000)
-    //   .subscribe
-    //   // Load solutionEvaluations entry
-    //   ();
-
     console.log("Darviewevaluation", this.dar);
-    // this.e$ == this.darevaluationService.findAllForDar(this.dar.id);
-    // console.log("Darviewevaluation e$", this.e$);
-    this.evaluations$$ = this.darevaluationService
+
+    this.darsolution$$ = this.darsolutionService
+      .findAllDarsolutions(this.dar.id, 1000)
+      .subscribe(dsa => (this.darsolutions = dsa));
+
+    this.darcriteria$$ = this.darcriteriaService
+      .findAllDarcriteria(this.dar.id, 1000)
+      .subscribe(dca => (this.darcriterias = dca));
+
+    this.darevaluation$$ = this.darevaluationService
       .findAllForDar(this.dar.id)
-      .subscribe(das => {
-        console.log("Darviewevaluation evaluations$$", das);
-      });
+      .subscribe(dea => (this.darevaluations = dea));
+  }
+
+  displayEvaluationCell(dsid: string, dcid: string): string {
+    console.log("displayEvaluationCell", dsid, dcid);
+    if (!dsid || !dcid) return " ";
+    const evaluation: Darevaluation = this.darevaluations.find(
+      e => e.id == dcid && e.dsid == dsid
+    );
+    if (evaluation) return evaluation.evaluationScore.toString();
+    else return "-";
   }
 
   ngOnDestroy() {
     if (this.darsolution$$) this.darsolution$$.unsubscribe();
+    if (this.darevaluation$$) this.darevaluation$$.unsubscribe();
+    if (this.darcriteria$$) this.darcriteria$$.unsubscribe();
   }
-}
-
-interface SolutionEvaluation {
-  dsid: string;
-  solutionName: string;
-  evaluation: { dcid: string; evaluationScore: EvaluationScore }[];
 }
