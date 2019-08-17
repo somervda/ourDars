60743aaf2918d3da33f619832030a1e939ae5f92
David <39265971+somervda@users.noreply.github.com>
2019-08-17 11:25:04 -0400

    Refactored myDars logic with a switchMap

--- src/app/mydars/mydars.component.ts
+++ src/app/mydars/mydars.component.ts
@@ -1,35 +1,24 @@
-import {
-  Component,
-  OnInit,
-  AfterViewInit,
-  ViewChild,
-  OnDestroy
-} from "@angular/core";
-import { DarsDataSource } from "../services/dars.datasource";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { DarStatus, Dar } from "../models/dar.model";
-import { MatSort } from "@angular/material";
 import { DarService } from "../services/dar.service";
-import { tap } from "rxjs/operators";
+import { switchMap } from "rxjs/operators";
 import { Observable, Subscription } from "rxjs";
 import { AuthService } from "../services/auth.service";
 import { Kvp } from "../models/global.model";
 import { enumToMap } from "../shared/utilities";
-import { THIS_EXPR } from "@angular/compiler/src/output/output_ast";
 
 @Component({
   selector: "app-mydars",
   templateUrl: "./mydars.component.html",
   styleUrls: ["./mydars.component.scss"]
 })
-export class MydarsComponent implements OnInit, OnDestroy {
+export class MydarsComponent implements OnInit {
   @ViewChild("selectedDarStatus") selectedDarStatus;
   @ViewChild("selectedRole") selectedRole;
   dars$: Observable<Dar[]>;
   displayedColumns = ["title", "darStatus", "roles", "actions"];
   darStatus = DarStatus;
   darStatuses: Kvp[];
-  user$$: Subscription;
-  uid: string;
 
   constructor(private darService: DarService, private auth: AuthService) {}
 
@@ -38,9 +27,7 @@ export class MydarsComponent implements OnInit, OnDestroy {
     this.selectedDarStatus.value = "";
     this.selectedRole.value = "";
     this.updateQuery();
-
     this.darStatuses = enumToMap(DarStatus);
-    //this.dataSource.loadDars("", "title", "asc", 100);
   }
 
   updateQuery() {
@@ -52,20 +39,21 @@ export class MydarsComponent implements OnInit, OnDestroy {
     //   "-",
     //   this.selectedDarStatus.value
     // );
-    if (this.user$$) this.user$$.unsubscribe();
-    this.user$$ = this.auth.user$.subscribe(u => {
-      this.dars$ = this.darService.findMyDars(
-        u.uid,
-        this.selectedRole.value,
-        this.selectedDarStatus.value == ""
-          ? undefined
-          : this.selectedDarStatus.value,
-        100
-      );
-    });
-  }
 
-  ngOnDestroy() {
-    if (this.user$$) this.user$$.unsubscribe();
+    // Use switchMap to wait for uid to be available before
+    // creating dars$ observable - needed when doing a browser refresh (auth gets delayed)
+    this.dars$ = this.auth.user$.pipe(
+      switchMap(
+        u =>
+          (this.dars$ = this.darService.findMyDars(
+            u.uid,
+            this.selectedRole.value,
+            this.selectedDarStatus.value == ""
+              ? undefined
+              : this.selectedDarStatus.value,
+            100
+          ))
+      )
+    );
   }
 }
