8fe50d5edb8ff9d704509ba05d82ffa898651dbb
David <39265971+somervda@users.noreply.github.com>
2019-08-20 10:23:49 -0400

    wip - working getNextStatus function - stll need real code

--- functions/src/darStatus.ts
+++ functions/src/darStatus.ts
@@ -1,5 +1,70 @@
+//import * as functions from "firebase-functions";
+import { db } from "./init";
 
-export function onGetNextDarStatus(did: string): string {
- 
-    return "Hi-" + did + "-";
-  }
+export enum DarStatus {
+  evaluate = 1,
+  vote = 2,
+  confirm = 3,
+  closed = 4,
+  create = 5,
+  deleted = 6
+}
+
+export enum DarMethod {
+  // Vote : Dar users vote on best solution (Political process)
+  Vote = 1,
+  // Process : A standard DAR process is used that includes defining solutions, criteria and scoring options
+  // before solutions are independently evaluated. Solution with the highest score is chosen
+  Process = 2,
+  // Hybrid : A process of scoring is used but final decision is still based on a vote, highest
+  // scored solution may not be chosen
+  Hybrid = 3
+}
+
+export interface DarNextStatusInfo {
+  darStatus: DarStatus | null;
+  darMethod: DarMethod | null;
+  nextDarStatus: DarStatus | null;
+  nextDarStatusExplanation: string;
+  comments: string;
+}
+
+export async function onGetNextDarStatus(
+  did: string
+): Promise<DarNextStatusInfo> {
+  // Will return the valid next DarStatus or null if no valid next status
+  // Next status is based on the current DarStatus, methodology, the content of darsolution,darcriteria and daruser
+  let darNextStatusInfo = <DarNextStatusInfo>{
+    darStatus: null,
+    darMethod: null,
+    nextDarStatus: null,
+    nextDarStatusExplanation: "",
+    comments: ""
+  };
+
+  console.log("onGetNextDarStatus did", did);
+  const docRef = db.collection("dars").doc(did);
+
+  await docRef
+    .get()
+    .then(function(doc: any) {
+      if (doc.exists) {
+        const dar = doc.data();
+        console.log("Document data:", dar);
+        darNextStatusInfo.darMethod = dar.darMethod;
+        darNextStatusInfo.darStatus = dar.darStatus;
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    })
+    .catch(function(error: any) {
+      console.log("Error getting document:", error);
+    });
+
+  // Logic to work out next available status.
+  // For documents in create status additional work is needed
+  // to check darusers, darcriteria and dars
+
+  return darNextStatusInfo;
+}
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import { onGetNextDarStatus } from  "./darStatus";
+import { onGetNextDarStatus } from "./darStatus";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -31,14 +31,19 @@ exports.darsDateCreated = functions.firestore
     );
   });
 
-export const getNextDarStatus = functions.https.onRequest((request, response) => {
+export const getNextDarStatus = functions.https.onRequest(
+  async (request, response) => {
     // Use to test the onGetNextDarStatus function
-    console.log("request.params",request.params['0']);
-    console.log("request.params onGetNextDarStatus ",onGetNextDarStatus(request.params['0']));
-    
-    
-  response.send(onGetNextDarStatus(request.params['0']));
-});
+    console.log("request.params", request.params["0"]);
+    let pathParam = <string>request.params["0"];
+    if (pathParam.slice(0, 1) == "/") pathParam = pathParam.substr(1);
+    let x;
+
+    x = await onGetNextDarStatus(pathParam);
+
+    response.send(pathParam + "-" + x.darStatus);
+  }
+);
 
 export {
   onCreateDarUser,
@@ -47,4 +52,3 @@ export {
 } from "./darUsersIndex";
 
 export { onCreateDarEvaluations } from "./darEvaluations";
-
