aaf1d4397c0feb9601952978cdacfad8e63cf58c
David <39265971+somervda@users.noreply.github.com>
2019-09-10 15:24:03 -0400

    wip - not working import export

--- src/app/darimport/darimport.component.ts
+++ src/app/darimport/darimport.component.ts
@@ -12,10 +12,7 @@ import { DarcriteriaService } from "../services/darcriteria.service";
 })
 export class DarimportComponent implements OnInit {
   @ViewChild("txtJSON", { static: true }) txtJSON;
-  newDarRef: firebase.firestore.DocumentReference;
-  did = {} as oldNew;
-  dcids = [] as oldNew[];
-  dsids = [] as oldNew[];
+  log = "";
 
   constructor(
     private darService: DarService,
@@ -29,98 +26,138 @@ export class DarimportComponent implements OnInit {
 
   async onImport() {
     console.log("JSON:", this.txtJSON.nativeElement.value);
-    const darJson = JSON.parse(this.txtJSON.nativeElement.value);
-    console.log("darJson:", darJson);
+    try {
+      const darJson = JSON.parse(this.txtJSON.nativeElement.value);
 
-    // As dar documents are imported , update the document Ids
+      console.log("darJson:", darJson);
 
+      await this.importDar(darJson)
+        .then(s => (this.log = s))
+        .catch(error => console.error("Error importing dar:", error));
+    } catch (error) {
+      this.log += " Error reading DAR JSON:" + error;
+    }
+  }
+
+  async importDar(darJson): Promise<string> {
+    let did = {} as oldNew;
+    let dcids = [] as oldNew[];
+    let dsids = [] as oldNew[];
+
+    let log = "";
     // Create the dar
-    this.did.old = darJson.dar.id;
+    did.old = darJson.dar.id;
     delete darJson.dar.id;
     await this.darService
       .createDar(darJson.dar)
       .then(darRef => {
         console.log("darRef", darRef);
-        this.newDarRef = darRef;
-        this.did.new = this.newDarRef.id;
+        did.new = darRef.id;
+        log += " Added dar:" + did.new.toString();
       })
-      .catch(error => console.error("Error creating Dar:", error));
+      .catch(error => {
+        console.error("Error creating Dar:", error);
+        log += " Failed creating new DAR document:" + error;
+        return log;
+      });
 
     // Create criteria
-    darJson.darCriteria.forEach(async darCriteria => {
+    await darJson.darCriteria.forEach(async darCriteria => {
       let dcid: oldNew = { old: darCriteria.id, new: "" };
       delete darCriteria.id;
       await this.darcriteriaService
-        .createDarcriteria(this.did.new, darCriteria)
+        .createDarcriteria(did.new, darCriteria)
         .then(darcriteriaRef => {
           console.log("darcriteriaRef", darcriteriaRef);
           dcid.new = darcriteriaRef.id;
-          this.dcids.push(dcid);
+          dcids.push(dcid);
+          log +=
+            " Created new DARcriteria document:" + dcid.old + "-" + dcid.new;
         })
-        .catch(error =>
-          console.error("Error creating Darcriteria:", dcid, error)
-        );
+        .catch(error => {
+          console.error("Error creating Darcriteria:", error);
+          log += " Failed creating new DARcriteria document:" + error;
+          return log;
+        });
     });
 
     // Create solutions
-    darJson.darSolutions.forEach(async darSolution => {
+    await darJson.darSolutions.forEach(async darSolution => {
       let dsid: oldNew = { old: darSolution.id, new: "" };
       delete darSolution.id;
       await this.darsolutionService
-        .createDarsolution(this.did.new, darSolution)
+        .createDarsolution(did.new, darSolution)
         .then(darsolutionRef => {
           console.log("darsolutionRef", darsolutionRef);
           dsid.new = darsolutionRef.id;
-          this.dsids.push(dsid);
+          dsids.push(dsid);
+          log +=
+            " Created new Darsolution document:" + dsid.old + "-" + dsid.new;
         })
-        .catch(error =>
-          console.error("Error creating Darsolution:", dsid, error)
-        );
+        .catch(error => {
+          console.error("Error creating Darsolution:", error);
+          log += " Failed creating new DARsolution document:" + error;
+          return log;
+        });
     });
 
     // Create darUsers, no changes to the darUser.id
-    darJson.darUsers.forEach(async darUser => {
+    await darJson.darUsers.forEach(async darUser => {
       // update the vote dsid if matches one of the imported solution ids
-      if (this.dsids.find(s => s.old == darUser.solutionVote.dsid)) {
-        darUser.solutionVote.dsid = this.dsids.find(
+      if (dsids.find(s => s.old == darUser.solutionVote.dsid)) {
+        darUser.solutionVote.dsid = dsids.find(
           s => s.old == darUser.solutionVote.dsid
         ).new;
       }
       await this.daruserService
-        .createDaruser(this.did.new, darUser.id, darUser)
+        .createDaruser(did.new, darUser.id, darUser)
         .then(() => {
+          log += " Created new Daruser document:" + darUser.id;
           console.log("daruserRef", darUser);
         })
-        .catch(error =>
-          console.error("Error creating darUser:", darUser, error)
-        );
+        .catch(error => {
+          console.error("Error creating Daruser:", error);
+          log += " Failed creating new DARuser document:" + error;
+          return log;
+        });
     });
 
     // Create evaluations
-    darJson.darEvaluations.forEach(async darEvaluation => {
-      const dsid = this.dsids.find(e => e.old == darEvaluation.dsid).new;
-      const dcid = this.dcids.find(e => e.old == darEvaluation.dcid).new;
+    await darJson.darEvaluations.forEach(async darEvaluation => {
+      const dsid = dsids.find(e => e.old == darEvaluation.dsid).new;
+      const dcid = dcids.find(e => e.old == darEvaluation.dcid).new;
       darEvaluation["id"] = dcid;
-      darEvaluation["did"] = this.did.new;
+      darEvaluation["did"] = did.new;
       darEvaluation["dsid"] = dsid;
       await this.darevaluationService
-        .setEvaluation(this.did.new, dsid, dcid, darEvaluation)
+        .setEvaluation(did.new, dsid, dcid, darEvaluation)
         .then(() => {
+          log +=
+            " Created new Darevaluation document, dsid:" +
+            dsid +
+            " dcid:" +
+            dcid;
           console.log("darevaluationRef", darEvaluation);
         })
-        .catch(error =>
-          console.error("Error creating Darevaluation:", darEvaluation, error)
-        );
+        .catch(error => {
+          console.error("Error creating Darevaluation:", error);
+          log += " Failed creating new Darevaluation document:" + error;
+          return log;
+        });
     });
 
     //Final cleanup, update dsid if it matches one of the solutions imported
-    if (this.dsids.find(s => s.old == darJson.dar.dsid)) {
+    if (dsids.find(s => s.old == darJson.dar.dsid)) {
       this.darService.fieldUpdate(
-        this.did.new,
+        did.new,
         "dsid",
-        this.dsids.find(s => s.old == darJson.dar.dsid).new
+        dsids.find(s => s.old == darJson.dar.dsid).new
       );
+      log += " Cleaned up dar.dsid.";
     }
+
+    console.log("end of dar import", log);
+    return log;
   }
 }
 
